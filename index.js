5ea57f061fb85b31fdc9fb0c7eac97752e8beb89
Marko <105108389+MrKMark202@users.noreply.github.com>
2024-04-05 21:17:52 +0200

    Update 0.1.2.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express  from "express";
 import cors from "cors";
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import cookieParser from "cookie-parser";
 
 const app = express()
 app.get('/', (req, res) => {
